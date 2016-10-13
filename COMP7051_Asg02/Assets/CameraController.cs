359936df8e2a5ec3be98ec2be1ec646ff53921d3
Denis Turitsa <dturitsa@gmail.com>
2016-10-13 02:28:06 -0700

    player movement. Free camera. optional collisions

---
+++ COMP7051_Asg02/Assets/CameraController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public Transform cameraOrigin;
+    public float camRotationSpeed = 2;
+    public float zoomSpeed = 20;
+   
+    private Vector3 offset;
+    private Vector3 originalPos;
+   
+    // Use this for initialization
+    void Start() {
+        offset = transform.position - cameraOrigin.position;
+        originalPos = cameraOrigin.InverseTransformPoint(transform.position);
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Zoom") != 0) {
+            transform.Translate(cameraOrigin.position * Input.GetAxis("Zoom") * zoomSpeed * Time.deltaTime);
+            offset = transform.position - cameraOrigin.position;
+            //offset = originalOffset;
+        }
+        
+    }
+
+    void LateUpdate() {
+
+        if (Input.GetAxis("CamRotate") != 0) {
+            offset = transform.position - cameraOrigin.position;
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * camRotationSpeed, Vector3.up) * offset;
+            transform.position = cameraOrigin.position + offset;
+
+            offset = Quaternion.AngleAxis(-Input.GetAxis("Mouse Y") * camRotationSpeed, Vector3.right) * offset;
+            transform.position = cameraOrigin.position + offset;
+            transform.LookAt(cameraOrigin.position);
+        }
+        else {
+            transform.position = cameraOrigin.TransformPoint(originalPos);
+            transform.LookAt(cameraOrigin.position);
+        }
+ 
+    }
+}
---
+++ COMP7051_Asg02/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Animator anim;
+    private CharacterController controller;
+    public float turnSpeed = 100;// how fast the character can turn
+    public float walkSpeed = 5; //character walking speed
+    public float runSpeed = 10;// character running speed
+    private Rigidbody rb;
+    private bool detectCollisions = true;
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent<Animator>();
+        if (anim == null)
+            Debug.Log("no animator found on" + gameObject.name);
+        controller = GetComponent<CharacterController>();
+        if (anim == null)
+            Debug.Log("no CharacterController found on" + gameObject.name);
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Vertical") > .1)
+            anim.SetFloat("forwardMotion", Input.GetAxis("Vertical"));
+        else
+            anim.SetFloat("forwardMotion", 0);
+
+        bool isRunning = false;
+        if (Input.GetAxis("Run") > 0)
+            isRunning = true;
+
+        anim.SetBool("run", isRunning);
+
+
+
+        if(Input.GetAxis("Vertical") > 0 && !isRunning)
+            transform.Translate(transform.forward * Input.GetAxis("Vertical") * walkSpeed * Time.deltaTime, Space.World);
+        else if(Input.GetAxis("Vertical") > 0 && isRunning)
+            transform.Translate(transform.forward * Input.GetAxis("Vertical") * runSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetAxis("Horizontal") != 0) {
+            transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+            anim.SetFloat("forwardMotion", 1);
+        }
+
+        if (Input.GetKeyDown("u")) {
+            Debug.Log("hi");
+            detectCollisions = !detectCollisions;
+            rb.isKinematic = !detectCollisions;
+            
+        }
+        
+    }
+}
